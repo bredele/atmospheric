3bf8d62619560884f73bcb32a5b9f3e8db0c8a9e
bredele <olivier.wietrich@gmail.com>
2014-09-21 20:36:19 -0600

    add none tested version

---
+++ index.js
@@ -0,0 +1,13 @@
+
+/**
+ * Atmospheric pressure.
+ *
+ * @param {m} altitude
+ * @return {hPa}
+ */
+
+module.exports = function(altitude) {
+  return 1013.25 * Math.pow((1 - 0.0065 * altitude / 288.15), 5.255);
+};
+
+
